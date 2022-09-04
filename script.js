ef364087a07e163816ef60f054b50a7ea0b9317c
ozerkip <ozerkip@hotmail.de>
2022-09-04 19:08:20 +0200

    organized and added more js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let displayValue = ""
+let newValue = ""
+let operator
+
+
 function add (a,b){
   return a + b 
 }
@@ -24,15 +29,16 @@ function operate(operator,a,b){
     result=substract(a,b)
   }
     
-  else if(operator==='*'){
+  else if(operator==='x'){
     result=multiply(a,b)
   }
     
-  else if(operator==='/'){
+  else if(operator==='÷'){
      result=divide(a,b)
   }
   return result
 }
+
 
 let displayCont=document.querySelector('.display p')
 
@@ -60,26 +66,84 @@ let operateCont=document.querySelector('.operate')
 let num0Cont=document.querySelector('.zero')
 let commaCont=document.querySelector('.comma')
 
-let displayValue 
-let newValue = 0
-let operator
-let endValue = 0
 
 function showAtDisplay (){
-   displayCont.textContent+=  this.textContent
-   displayValue = Number(displayCont.textContent)
-   console.log(displayValue)
-   return displayValue  
+  console.log('displayvalue',displayValue)
+  console.log(typeof displayValue)
+  
+  if(displayValue !=""){
+    displayCont.textContent += this.textContent
+    console.log('displayCont', displayCont.textContent)
+    displayValue = displayCont.textContent
+  }
+
+  
+  
+  if(displayValue===""){
+    displayCont.textContent = this.textContent
+    console.log('displayCont', displayCont.textContent)
+    displayValue = displayCont.textContent
+  }
+  
+ 
+
+
+
+  console.log('newvalue',newValue)
+
+  console.log('displayvalue',displayValue)
+  console.log(typeof displayValue)
+
+ 
+    
+   
+  
+  
+
+  
+   
+  
+  
+
   }
 
 function clearFromDisplay (){
   displayCont.textContent = ""
-  //displayValue 
-  newValue = 0;
-  endValue = 0;
-  return displayValue 
+  newValue = "";
+  
+  
+ 
 }
 
+
+function saveValuesAndOperate () {
+  // if i dont have a display value (cleared, started new or clicked already an operator...) then do nothing (..dont save or operate)
+  if(displayValue===""){
+    return
+  }
+
+  // clicking the first time, there is no new value, so new value becomes displayValue
+  if(newValue===""){
+   newValue = Number(displayValue) 
+   displayCont.textContent = newValue
+  }
+
+  // clicking after i have a new value, i can operate..
+  else {
+    operate(operator, newValue, Number(displayValue))
+    newValue=result
+    displayCont.textContent = newValue
+  }
+
+  // saving operator and clearing the displayValue at last place to not have issues with the if statements
+    // 
+  operator = this.textContent
+  console.log(operator)
+  console.log('newValue', newValue)
+  displayValue = ""
+  console.log('displayvalue', displayValue)
+
+}
 
 clearCont.addEventListener('click', clearFromDisplay)
 num1Cont.addEventListener('click', showAtDisplay)
@@ -94,10 +158,21 @@ num9Cont.addEventListener('click', showAtDisplay)
 num0Cont.addEventListener('click', showAtDisplay)
 commaCont.addEventListener('click', showAtDisplay)
 
+let result = ""
+
+divideCont.addEventListener('click', saveValuesAndOperate)
+multiplyCont.addEventListener('click', saveValuesAndOperate)
+addCont.addEventListener('click', saveValuesAndOperate)
+substractCont.addEventListener('click', saveValuesAndOperate)
 
 
-addCont.addEventListener('click', function(){
-  operator = '+'
+
+
+
+
+/*function(){
+
+  operator = this.textContent
   console.log(operator)
   newValue = displayValue
   console.log(newValue)
@@ -111,13 +186,22 @@ addCont.addEventListener('click', function(){
   return endValue
  
 })
-
+*/
 
 operateCont.addEventListener('click', function(){
-  operate(operator, endValue, displayValue)
+ 
+
+  operate(operator, newValue, Number(displayValue))
     console.log(operator)
+   
     displayCont.textContent = result
+    newValue = result
     console.log(result)
+    
+
+    
+    console.log('displayvalue',displayValue)    
+    console.log('newValue', newValue)
   //displayCont.textContent = newValue
   //console.log(newValue)
 })
